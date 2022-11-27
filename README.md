14f11ea5b024f2b8e51ab08f8a1d07718f9f2678
fahmyxfiles <fahmi-tamar@thebodyshop.co.id>
2022-11-27 18:00:23 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,59 @@
+# Traefik Modsecurity Redirect
+
+![Banner](./img/banner.png)
+
+Traefik plugin to proxy requests to [owasp/modsecurity-crs](https://hub.docker.com/r/owasp/modsecurity-crs):apache
+
+![Github Actions](https://img.shields.io/github/workflow/status/acouvreur/traefik-modsecurity-plugin/Build?style=flat-square)
+![Go Report](https://goreportcard.com/badge/github.com/acouvreur/traefik-modsecurity-plugin?style=flat-square)
+![Go Version](https://img.shields.io/github/go-mod/go-version/acouvreur/traefik-modsecurity-plugin?style=flat-square)
+![Latest Release](https://img.shields.io/github/release/acouvreur/traefik-modsecurity-plugin/all.svg?style=flat-square)
+
+
+- [Traefik Modsecurity Redirect](#traefik-modsecurity-redirect)
+  - [Demo](#demo)
+  - [Usage (docker-compose.yml)](#usage-docker-composeyml)
+  - [How it works](#how-it-works)
+  - [Configuration](#configuration)
+  - [Local development (docker-compose.local.yml)](#local-development-docker-composelocalyml)
+
+## Demo
+
+Demo with WAF intercepting relative access in query param.
+
+![Demo](./img/waf.gif)
+
+## Usage (docker-compose.yml)
+
+See [docker-compose.yml](docker-compose.yml)
+
+1. docker-compose up
+2. Go to http://localhost:8000/website, the request is received without warnings
+3. Go to http://localhost:8000/website?test=../etc, the request is intercepted and returned with 403 Forbidden by owasp/modsecurity
+
+## How it works
+
+This is a very simple plugin that proxies the query to the owasp/modsecurity apache container.
+
+The plugin checks that the response from the waf container hasn't an http code > 400 before forwarding the request to the real service.
+
+If it is > 400, then the error page is returned instead.
+
+The *dummy* service is created so the waf container forward the request to a service and respond with 200 OK all the time.
+
+## Configuration
+
+This plugin supports these configuration:
+
+* `modSecurityUrl`: (**mandatory**) it's the URL for the owasp/modsecurity container.
+* `redirectUrl`: (**mandatory**) it's the URL for redirection if thread detected.
+* `maxBodySize`: (optional) it's the maximum limit for requests body size. Requests exceeding this value will be rejected using `HTTP 413 Request Entity Too Large`.
+  The default value for this parameter is 10MB. Zero means "use default value".
+
+**Note**: body of every request will be buffered in memory while the request is in-flight (i.e.: during the security check and during the request processing by traefik and the backend), so you may want to tune `maxBodySize` depending on how much RAM you have.
+
+## Local development (docker-compose.local.yml)
+
+See [docker-compose.local.yml](docker-compose.local.yml)
+
+`docker-compose -f docker-compose.local.yml up` to load the local plugin
---
+++ modsecurity.go
@@ -0,0 +1,148 @@
+// Package traefik_modsecurity_redirect a modsecurity plugin.
+package traefik_modsecurity_redirect
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Net client is a custom client to timeout after 2 seconds if the service is not ready
+var httpClient = &http.Client{
+	Timeout: time.Second * 2,
+}
+
+// Config the plugin configuration.
+type Config struct {
+	ModSecurityUrl string `json:"modSecurityUrl,omitempty"`
+	RedirectUrl string `json:"redirectUrl,omitempty"`
+	MaxBodySize    int64  `json:"maxBodySize"`
+}
+
+// CreateConfig creates the default plugin configuration.
+func CreateConfig() *Config {
+	return &Config{
+		// Safe default: if the max body size was not specified, use 10MB
+		// Note that this will break any file upload with files > 10MB. Hopefully
+		// the user will configure this parameter during the installation.
+		MaxBodySize: 10 * 1024 * 1024,
+	}
+}
+
+// Modsecurity a Modsecurity plugin.
+type Modsecurity struct {
+	next           http.Handler
+	modSecurityUrl string
+	redirectUrl	   string
+	maxBodySize    int64
+	name           string
+	logger         *log.Logger
+}
+
+// New created a new Modsecurity plugin.
+func New(ctx context.Context, next http.Handler, config *Config, name string) (http.Handler, error) {
+	if len(config.ModSecurityUrl) == 0 {
+		return nil, fmt.Errorf("modSecurityUrl cannot be empty")
+	}
+
+	if len(config.redirectUrl) == 0 {
+		return nil, fmt.Errorf("redirectUrl cannot be empty")
+	}
+
+	return &Modsecurity{
+		modSecurityUrl: config.ModSecurityUrl,
+		redirectUrl:    config.redirectUrl,
+		maxBodySize:    config.MaxBodySize,
+		next:           next,
+		name:           name,
+		logger:         log.New(os.Stdout, "", log.LstdFlags),
+	}, nil
+}
+
+func (a *Modsecurity) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+
+	// Websocket not supported
+	if isWebsocket(req) {
+		a.next.ServeHTTP(rw, req)
+		return
+	}
+
+	// we need to buffer the body if we want to read it here and send it
+	// in the request.
+	body, err := ioutil.ReadAll(http.MaxBytesReader(rw, req.Body, a.maxBodySize))
+	if err != nil {
+		if err.Error() == "http: request body too large" {
+			a.logger.Printf("body max limit reached: %s", err.Error())
+			http.Error(rw, "", http.StatusRequestEntityTooLarge)
+		} else {
+			a.logger.Printf("fail to read incoming request: %s", err.Error())
+			http.Error(rw, "", http.StatusBadGateway)
+		}
+		return
+	}
+
+	// you can reassign the body if you need to parse it as multipart
+	req.Body = ioutil.NopCloser(bytes.NewReader(body))
+
+	// create a new url from the raw RequestURI sent by the client
+	url := fmt.Sprintf("%s%s", a.modSecurityUrl, req.RequestURI)
+
+	proxyReq, err := http.NewRequest(req.Method, url, bytes.NewReader(body))
+
+	if err != nil {
+		a.logger.Printf("fail to prepare forwarded request: %s", err.Error())
+		http.Error(rw, "", http.StatusBadGateway)
+		return
+	}
+
+	// We may want to filter some headers, otherwise we could just use a shallow copy
+	// proxyReq.Header = req.Header
+	proxyReq.Header = make(http.Header)
+	for h, val := range req.Header {
+		proxyReq.Header[h] = val
+	}
+
+	resp, err := httpClient.Do(proxyReq)
+	if err != nil {
+		a.logger.Printf("fail to send HTTP request to modsec: %s", err.Error())
+		http.Error(rw, "", http.StatusBadGateway)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode >= 400 {
+		res.Header().Add("Location", a.redirectUrl)
+		res.WriteHeader(http.StatusTemporaryRedirect)
+		return
+	}
+
+	a.next.ServeHTTP(rw, req)
+}
+
+func isWebsocket(req *http.Request) bool {
+	for _, header := range req.Header["Upgrade"] {
+		if header == "websocket" {
+			return true
+		}
+	}
+	return false
+}
+
+func forwardResponse(resp *http.Response, rw http.ResponseWriter) {
+	// copy headers
+	for k, vv := range resp.Header {
+		for _, v := range vv {
+			rw.Header().Set(k, v)
+		}
+	}
+	// copy status
+	rw.WriteHeader(resp.StatusCode)
+	// copy body
+	io.Copy(rw, resp.Body)
+}
